6be31be7d195148a85b232676f73fe37dfe2eb28
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-11-07 19:50:08 +0530

    Update

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -39,7 +39,7 @@ function Dropdown() {
 
   const handleClick = (id) => {
     setActiveItem(id); // Set the clicked item as active
-    setDropdownToggled(false); // Close dropdown on item click
+    setDropdownToggled(false); // Close dropdown after clicking an item
   };
 
   const handleMouseEnter = (id) => {
@@ -53,33 +53,36 @@ function Dropdown() {
   return (
     <div className="md:hidden flex justify-center items-center drop-shadow-2xl">
       <button onClick={() => setDropdownToggled(!dropdownToggled)}>
-        <Hamburger size={30} color={textColor} />
+        <Hamburger size={30} color={textColor} toggled={dropdownToggled} />
       </button>
       {dropdownToggled && ( // Conditionally render the menu items
         <div
-          className="dropdown-menu absolute -right-3 top-16 flex justify-center text-center flex-col-reverse rounded-xl drop-shadow-2xl border-2"
+          className="dropdown-menu absolute -right-3 top-16 flex justify-center text-center flex-col-reverse rounded-xl drop-shadow-2xl border-2 overflow-hidden"
           style={{ backgroundColor: textColor, opacity: 0.9, color: color, borderColor: color }}
         >
           {menuItems.map((menu, index) => (
             <Link
-              ref={(el) => menuRefs.current[index] = el} // Assign individual refs to each menu item
-              className={`pb-1 pt-2 px-2 m-1 font-["Aero"] rounded-lg`}  // Always add hover effect
               key={menu.id}
               to={menu.href} // Use 'to' instead of 'href' for react-scroll
               smooth={true} // Enable smooth scrolling
-              duration={1000} // Duration for smooth scrolling
-              onClick={() => handleClick(menu.id)} // Set active item on click
-              onMouseEnter={() => handleMouseEnter(menu.id)} // Set hovered item on mouse enter
-              onMouseLeave={handleMouseLeave} // Reset hover item on mouse leave
-              style={{
-                backgroundColor:
-                  activeItem === menu.id ? color : hoverItem === menu.id ? color : '',
-                color: activeItem === menu.id ? textColor : hoverItem === menu.id ? textColor : '',
-                borderColor: hoverItem === menu.id ? color : '',
-                opacity: hoverItem === menu.id && activeItem !== menu.id ? 0.2 : 1, // Opacity for hovered item
-              }}
+              duration={1000} // Duration for smooth scrolling 
             >
-              {menu.label}
+              <div
+                onClick={() => handleClick(menu.id)} // Set active item on click
+                onMouseEnter={() => handleMouseEnter(menu.id)} // Set hovered item on mouse enter
+                onMouseLeave={handleMouseLeave} // Reset hover item on mouse leave
+                className={`pb-1 pt-2 px-2 m-1 font-["Aero"] rounded-lg`}  // Always add hover effect
+                style={{
+                  backgroundColor:
+                    activeItem === menu.id ? color : hoverItem === menu.id ? color : '',
+                  color: activeItem === menu.id ? textColor : hoverItem === menu.id ? textColor : '',
+                  borderColor: hoverItem === menu.id ? color : '',
+                  opacity: hoverItem === menu.id && activeItem !== menu.id ? 0.2 : 1, // Opacity for hovered item
+                }}
+                ref={(el) => menuRefs.current[index] = el} // Assign individual refs to each menu item
+              >
+                {menu.label}
+              </div>
             </Link>
           ))}
         </div>
