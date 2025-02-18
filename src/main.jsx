9307501893717921809a05e16338e1454c5e129c
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-02-18 23:25:20 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ColorProvider } from './context/ColorContext.jsx';
-
+import { Analytics } from '@vercel/analytics/react';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ColorProvider>
       <App />
+      <Analytics />
     </ColorProvider>
   </StrictMode>,
 );
