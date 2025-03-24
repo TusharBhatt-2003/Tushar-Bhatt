e052f826dc95ba644a0fcbeada621c715e44174c
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2025-03-24 15:06:03 +0530

    Update

--- src/Components/resume/ResumeCard/Resumecard.jsx
+++ src/Components/resume/ResumeCard/Resumecard.jsx
@@ -11,7 +11,7 @@ const ResumeCard = ({ title, items }) => {
     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
   };
 
-  const backgroundColorWithOpacity = hexToRgba(textColor, 0.5);
+  const backgroundColorWithOpacity = hexToRgba(textColor, 0.8);
 
   return (
     <>
